60b07d64c841b9135b09801c9a14d6ebbae04c2d
John Bintz <john@coswellproductions.com>
2011-03-30 09:19:15 -0400

    dumb change

--- lib/rspec/quickfix-formatter.rb
+++ lib/rspec/quickfix-formatter.rb
@@ -1,5 +1,3 @@
-require 'rspec/core/formatters/quick_fix_formatter'
-
 module RSpec
   class QuickFixFormatter < RSpec::Core::Formatters::BaseTextFormatter
     def dump_summary(duration, example_count, failure_count, pending_count)
